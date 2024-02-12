085bece45d4755fb64f37ff9abe3f7175d14abac
Yehonatan Mekayten <99586548+yehonatan768@users.noreply.github.com>
2024-02-12 13:16:40 +0200

    Update my_Knapsack.c

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -1,43 +1,3 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-
-#define MAX_ITEMS 5
-#define MAX_WEIGHT 20
-
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
-
-int knapSack(int weights[], int values[], int selected_bool[]) {
-    int i, w;
-    int K[MAX_ITEMS + 1][MAX_WEIGHT + 1];
-
-    // Initialize K matrix
-    for (i = 0; i <= MAX_ITEMS; i++) {
-        for (w = 0; w <= MAX_WEIGHT; w++) {
-            if (i == 0 || w == 0)
-                K[i][w] = 0;
-            else if (weights[i - 1] <= w)
-                K[i][w] = max(values[i - 1] + K[i - 1][w - weights[i - 1]], K[i - 1][w]);
-            else
-                K[i][w] = K[i - 1][w];
-        }
-    }
-
-    // Backtrack to find selected items
-    w = MAX_WEIGHT;
-    for (i = MAX_ITEMS; i > 0 && w > 0; i--) {
-        if (K[i][w] != K[i - 1][w]) {
-            selected_bool[i - 1] = true;
-            w -= weights[i - 1];
-        }
-    }
-
-    return K[MAX_ITEMS][MAX_WEIGHT];
-}
-
-
 int main() {
     int values[MAX_ITEMS];
     int weights[MAX_ITEMS];
@@ -47,8 +7,12 @@ int main() {
     // Input items' values and weights
     for (int i = 0; i < MAX_ITEMS; i++) {
         char item;
-        printf("Item %d: ", i + 1);
-        scanf(" %c %d %d", &item, &values[i], &weights[i]);
+        int value, weight;
+        scanf("%d", &value);
+        scanf("%d", &weight);
+
+        values[i] = value;
+        weights[i] = weight;
     }
 
     // Determine maximum profit and selected items
